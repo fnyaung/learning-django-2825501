bfd65928915d842f069188102995a87b0dad272b
fnyaung <fnyaung@gmail.com>
2021-08-05 20:27:56 -0700

    created Pet and Vaccine models

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pet(models.Model):
+    # The first element in each tuple is the actual value to be set on the model,
+    # and the second element is the human-readable name.
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
+
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
